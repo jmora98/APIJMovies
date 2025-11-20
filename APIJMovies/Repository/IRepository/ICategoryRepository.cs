c83a49613971ef382c6e6af7597bd8922f8a1445
jmora <jmora-ts@rapicredrit.com>
2025-11-19 22:59:09 -0500

    Add new interface and implementation.

---
+++ APIJMovies/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using APIJMovies.DAL.Models;
+
+namespace APIJMovies.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        Task<ICollection<Category>> GetCategoriesAsync(); // Me retornar  UNA LISTA DE CATEGORIAS
+        Task<Category> GetCategoryAsync (int id); // Me retorna una solo objeto de categories
+        Task<bool> CategoryExistsByIdAsync (int id); // me valida si existe una categoria por id true/false
+        Task<bool> GetCategoryByNameAsync (string name); // me valida si existe por nombre
+        Task<bool> CreateCategoryAsync (Category category);
+        Task<bool> UpdateCategoryAsync (Category category);
+        Task<bool> DeleteCategoryAsync (int id);
+
+    }
+}
